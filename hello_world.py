c8a7283f306c5b2e84fccb05a0bb8038b30129c6
lmnitzsche <logannitzsche1@gmail.com>
2024-01-24 03:37:07 -0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+def collatz_sequence(n):
+    steps = []
+    while n != 1:
+        steps.append(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+    steps.append(1)
+    return steps
+
+try:
+    start_number = int(input("Enter a positive integer to start the Collatz sequence: "))
+    if start_number <= 0:
+        print("Please enter a positive integer.")
+    else:
+        result = collatz_sequence(start_number)
+        print("Collatz sequence starting from {}: {}".format(start_number, result))
+except ValueError:
+    print("Invalid input. Please enter a valid positive integer.")
